740693e6d39c30f6e4c40126374a553d8f34e207
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-09-01 07:19:15 +0300

    01.09.2023 7:19

--- backend_2/data/management/commands/data_test.py
+++ backend_2/data/management/commands/data_test.py
@@ -3,8 +3,17 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 
 import os
+import csv
+
+
+from receipt.models import Tag, Ingredient
 
 TEST_DATA_FOLDER = settings.DATA_FOLDER / 'test_data'
+
+DATA_FILES = {
+    'tag': 'tags.csv',
+    'ingredient': 'ingredients.csv',
+}
 
 class Command(BaseCommand):
     help = 'тестирование'
@@ -12,6 +21,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print(settings.DATA_FOLDER)
         print(os.listdir(TEST_DATA_FOLDER))
+        load_tags()
+        load_ingredient()
+
+def load_tags():
+    with open(TEST_DATA_FOLDER / DATA_FILES['tag'], encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        Tag.objects.all().delete()
+        for row in reader:
+            Tag.objects.create(**row)
+
+
+def load_ingredient():
+    with open(TEST_DATA_FOLDER / DATA_FILES['ingredient'], encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        Ingredient.objects.all().delete()
+        for row in reader:
+            Ingredient.objects.create(**row)
+
 
 
 
