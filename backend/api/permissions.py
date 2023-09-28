79ac3f3b1b4760252086d1fc418894209da0f09b
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-09-28 23:17:46 +0300

    28.09.2023 23:17

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,14 +4,3 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsAuthorOrRedOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
-
-
-class IsAdminCurentUserOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or request.user.is_superuser
-                or obj.author == request.user)
