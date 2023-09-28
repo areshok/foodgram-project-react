d4ec2efe631aab6905fecbc759c0197f41aa4cd8
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-09-28 22:20:07 +0300

    28.09.2023 22:20

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -6,13 +6,12 @@ class IsAuthorOrRedOnly(BasePermission):
         return request.method in SAFE_METHODS or obj.author == request.user
 
 
-class IsAdminModeratorOwnerOrReadOnly(BasePermission):
+class IsAdminCurentUserOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_moderator
+                or request.user.is_superuser
                 or obj.author == request.user)
