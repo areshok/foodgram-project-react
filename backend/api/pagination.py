c119a484920d45e597fbc72a343e3a6807562ecd
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-09-28 21:51:46 +0300

    28.09.2023 21:51

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class ReceiptPagination(PageNumberPagination):
     page_size = 6
-    max_page_size = 10000
     page_size_query_param = 'limit'
