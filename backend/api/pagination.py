d919aa68ddf70e422266b3ffc9c1aa97689feee3
Arsenii Turlapov <arsturlapov@yandex.ru>
2023-09-28 21:06:58 +0300

    28.09.2023 21:06

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,4 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class ReceiptPagination(PageNumberPagination):
     page_size = 6
+    max_page_size = 10000
     page_size_query_param = 'page_size'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,8 +117,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',)
 
